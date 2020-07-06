b281c8db8d2e139666840ebbd18b272dc36a13f1
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-07-06 16:45:04 -0300

    Initial - ESLint and Prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles'
+
+const src: React.FC = () => (
+  <View>
+    <Text>Hello World!</Text>
+  </View>
+);
+
+export default src;
