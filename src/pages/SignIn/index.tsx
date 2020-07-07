2281d7067dc38544f62ed556ed9497b1ad8c28ac
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-07-07 17:12:06 -0300

    usability - keyboard and input focus

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useCallback } from 'react';
 import {
-  Image, View, ScrollView, KeyboardAvoidingView, Platform,
+  Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -18,6 +18,7 @@ import {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
   const handleSubmit = useCallback((data: object) => {
@@ -39,8 +40,25 @@ const SignIn: React.FC = () => {
             </View>
 
             <Form ref={formRef} onSubmit={handleSubmit}>
-              <Input name="email" icon="mail" placeholder="E-mail" />
-              <Input name="password" icon="lock" placeholder="Senha" />
+              <Input
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => { passwordInputRef.current?.focus(); }}
+              />
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
 
               <Button onPress={() => formRef.current?.submitForm()}>
                 Entrar
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useCallback } from 'react';
 import {
   Image, View, ScrollView, KeyboardAvoidingView, Platform,
+  TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -19,6 +20,8 @@ const SignUp: React.FC = () => {
   const navigation = useNavigation();
 
   const formRef = useRef<FormHandles>(null);
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
 
   const handleSubmit = useCallback((data: object) => {
     console.log(data);
@@ -39,9 +42,35 @@ const SignUp: React.FC = () => {
             </View>
 
             <Form ref={formRef} onSubmit={handleSubmit}>
-              <Input name="name" icon="user" placeholder="Nome" />
-              <Input name="email" icon="mail" placeholder="E-mail" />
-              <Input name="password" icon="lock" placeholder="Senha" />
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => { emailInputRef.current?.focus(); }}
+              />
+              <Input
+                ref={emailInputRef}
+                autoCorrect={false}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => { passwordInputRef.current?.focus(); }}
+              />
+              <Input
+                ref={passwordInputRef}
+                textContentType="newPassword"
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
 
               <Button onPress={() => formRef.current?.submitForm()}> Cadastrar</Button>
             </Form>
