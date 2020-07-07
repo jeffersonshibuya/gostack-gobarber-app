28063a2e6256e4017413d2f5b4811947497aaab3
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-07-07 11:39:28 -0300

    SignIn layout, Input and Button components, added and configured Feather Icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { View, Text, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 
-import Routes from './routes'
+import Routes from './routes';
 
 const src: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor='#312e38' />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View style={{ backgroundColor: '#312e38', flex: 1 }}>
       <Routes />
     </View>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignIn: React.FC = () => {
-  return <Container></Container>
-}
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+    <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button>Entrar</Button>
+  </Container>
+);
 
 export default SignIn;
