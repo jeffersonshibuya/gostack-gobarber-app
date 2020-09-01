f17f2f3a3af54e457e1c186b53059f4a3b327785
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-09-01 07:46:11 -0300

    deploy - play store

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
-import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const src: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
-    <AppProvider>
-      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    // Realizar qualquer necessária antes da splash screen ser removida
 
-export default src;
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        translucent
+      />
+      <AppProvider>
+        <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
+
+export default App;
